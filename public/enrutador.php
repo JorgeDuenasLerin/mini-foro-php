ff06e4898cc963517b8820c72eaa4f586e5d451a
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2019-12-12 19:36:18 +0100

    El enrutador acepta otros tipos de ficheros

--- public/enrutador.php
+++ public/enrutador.php
@@ -8,15 +8,16 @@ if (preg_match('/\.(?:css|js|ico|png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"]))
     if(startsWith($_SERVER["REQUEST_URI"], $config['img_in_url'])) {
         // Imagen subida por el usuario
 
-        // Solo aceptamos PNG
-        header('Content-Type: image/png');
-
         // Quitamos subir de directorio
         $file_path = str_replace("..","",$_SERVER["REQUEST_URI"]);
         // Quitamos el prefijo de la petición
         $file_path = str_replace($config['img_in_url'],"",$_SERVER["REQUEST_URI"]);
         // Cargamos el fichero y lo enviamos
-        readfile($ROOT.$config['img_path'].urldecode($file_path));
+
+        $file_path = $ROOT.$config['img_path'].urldecode($file_path);
+        $mime_type = mime_content_type($file_path);
+        header("Content-Type: $mime_type");
+        readfile($file_path);
 
     } else {
         return false;    // servir la petición tal cual es.
